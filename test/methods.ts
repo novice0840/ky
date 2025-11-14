2131ddccc2f7ff1bbf7a6a6663ccac7d3d544eb1
novice0840 <magnolia0840@gmail.com>
2025-11-14 21:30:34 +0900

    test: fix wrong test

--- test/methods.ts
+++ test/methods.ts
@@ -1,85 +1,81 @@
-import test from 'ava';
-import ky from '../source/index.js';
-import {createHttpTestServer} from './helpers/create-http-test-server.js';
+import test from "ava";
+import ky from "../source/index.js";
+import { createHttpTestServer } from "./helpers/create-http-test-server.js";
 
-test('common method is normalized', async t => {
-	const server = await createHttpTestServer();
-	server.all('/', (_request, response) => {
-		response.end();
-	});
+test("common method is normalized", async (t) => {
+  const server = await createHttpTestServer();
+  server.all("/", (_request, response) => {
+    response.end();
+  });
 
-	await t.notThrowsAsync(
-		ky(server.url, {
-			method: 'get',
-			hooks: {
-				beforeRequest: [
-					(_input, options) => {
-						t.is(options.method, 'GET');
-					},
-				],
-			},
-		}),
-	);
+  await t.notThrowsAsync(
+    ky(server.url, {
+      method: "get",
+      hooks: {
+        beforeRequest: [
+          (_input, options) => {
+            t.is(options.method, "GET");
+          },
+        ],
+      },
+    })
+  );
 
-	await server.close();
+  await server.close();
 });
 
-test('method defaults to "GET"', async t => {
-	const server = await createHttpTestServer();
-	server.all('/', (_request, response) => {
-		response.end();
-	});
+test('method defaults to "GET"', async (t) => {
+  const server = await createHttpTestServer();
+  server.all("/", (_request, response) => {
+    response.end();
+  });
 
-	t.plan(2);
+  t.plan(2);
 
-	await t.notThrowsAsync(
-		ky(server.url, {
-			hooks: {
-				beforeRequest: [
-					(_input, options) => {
-						t.is(options.method, 'GET');
-					},
-				],
-			},
-		}),
-	);
+  await t.notThrowsAsync(
+    ky(server.url, {
+      hooks: {
+        beforeRequest: [
+          (_input, options) => {
+            t.is(options.method, "GET");
+          },
+        ],
+      },
+    })
+  );
 
-	await server.close();
+  await server.close();
 });
 
-test.failing('custom method remains identical', async t => {
-	const server = await createHttpTestServer();
-	server.all('/', (_request, response) => {
-		response.end();
-	});
+test("custom method remains identical", async (t) => {
+  const server = await createHttpTestServer();
+  server.all("/", (_request, response) => {
+    response.end();
+  });
 
-	t.plan(1);
+  t.plan(1);
 
-	await t.notThrowsAsync(
-		// TODO: Is it correct for this to throw 400 status code?
-		ky(server.url, {
-			method: 'report',
-			hooks: {
-				beforeRequest: [
-					(_input, options) => {
-						t.is(options.method, 'report');
-					},
-				],
-			},
-		}),
-	);
-
-	await server.close();
+  ky(server.url, {
+    method: "report",
+    hooks: {
+      beforeRequest: [
+        (_input, options) => {
+          t.is(options.method, "report");
+        },
+      ],
+    },
+  });
+  await server.close();
 });
 
-test('shortcut headers have correct accept headers set', async t => {
-	const server = await createHttpTestServer();
-	server.all('/', (request, response) => {
-		t.is(request.headers.accept, 'text/*');
-		response.end('whatever');
-	});
+test("shortcut headers have correct accept headers set", async (t) => {
+  const server = await createHttpTestServer();
+  server.all("/", (request, response) => {
+    t.is(request.headers.accept, "text/*");
+    response.end("whatever");
+  });
 
-	await ky.get(server.url).text();
+  await ky.get(server.url).text();
 
-	await server.close();
+  await server.close();
 });
